ccfa23d111aeb817aa6f97d2fc929c26d0e18c08
ahmedsami019 <easami019192@gmail.com>
2025-11-30 21:31:50 +0600

    added signin with google features

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from '../../firebase.init';
 
 const Login = () => {
+
+    const provider = new GoogleAuthProvider()
+
+    const handleGoogleSignIn = ()=> {
+        console.log("login with google clicked");
+        signInWithPopup(auth, provider)
+        .then(result => {
+            console.log(result);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
     return (
         <div>
             <h2>this is login page</h2>
+            <h3 className='text-3xl'>Please Login </h3>
+            <button onClick={handleGoogleSignIn} className="btn">login with google</button>
         </div>
     );
 };
