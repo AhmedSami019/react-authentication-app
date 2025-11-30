73e498f9f405cbfdc486a7b06893ec20cdafc314
ahmedsami019 <easami019192@gmail.com>
2025-11-30 21:59:06 +0600

    display user data after login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,28 +1,57 @@
-import React from 'react';
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { auth } from '../../firebase.init';
+import React, { useState } from "react";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { auth } from "../../firebase.init";
 
 const Login = () => {
+  const provider = new GoogleAuthProvider();
 
-    const provider = new GoogleAuthProvider()
+  const [user, setUser] = useState(null);
 
-    const handleGoogleSignIn = ()=> {
-        console.log("login with google clicked");
-        signInWithPopup(auth, provider)
-        .then(result => {
-            console.log(result);
-        }).catch(error => {
-            console.log(error);
-        })
-    }
+  const handleGoogleSignIn = () => {
+    console.log("login with google clicked");
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    return (
+  const handleSignOut = () => {
+    console.log("sing out completed");
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <h2>this is login page</h2>
+      <h3 className="text-3xl">Please Login </h3>
+      {user ? (
+        <button onClick={handleSignOut} className="btn">
+          Sign out
+        </button>
+      ) : (
+        <button onClick={handleGoogleSignIn} className="btn">
+          login with google
+        </button>
+      )}
+
+      {user && (
         <div>
-            <h2>this is login page</h2>
-            <h3 className='text-3xl'>Please Login </h3>
-            <button onClick={handleGoogleSignIn} className="btn">login with google</button>
+          <h3>{user.displayName}</h3>
+          <p>{user.email}</p>
+          <img className="mx-auto" src={user.photoURL} alt="" />
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Login;
